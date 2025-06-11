b1e31f54f013c1301fac0dc7e5e9203fc9f2e34c
dante <dellatogna.io@gmail.com>
2025-06-11 12:27:55 -0500

    added convert2nifti.py

---
+++ convert2nifti.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import os
+
+# input_dir = "C:/Users/dante/OneDrive/Documentos/Projects/Parkinson-MRI/dataset/control_group_t1_axial_new/PPMI"
+input_dir = "C:/Users/dante/OneDrive/Documentos/datasets/pd_group_t1/PPMI"
+output_dir = (
+    "C:/Users/dante/OneDrive/Documentos/Projects/Parkinson-MRI/dataset/t1/pd_group_t1"
+)
+# os.system(f"cd ./{input_dir}")
+print(input_dir)
+
+count = 0
+
+for subject_path in os.listdir(input_dir):
+    # os.system(f"cd ./{subject_path}")
+    is_dcm = False
+    dmc_folder = ""
+    current_folder = f"{input_dir}/{subject_path}"
+    print(current_folder)
+    while is_dcm == False:
+        print(current_folder)
+        for file in os.listdir(current_folder):
+            if file.endswith(".dcm"):
+                is_dcm = True
+                dcm_fold = current_folder
+                print("aaaaaaaa")
+                break
+
+        if is_dcm == True:
+            os.system(f"dcm2niix -o {output_dir} {dcm_fold}/")
+        else:
+            next_folder = os.listdir(current_folder)[0]
+            # os.system(f"cd ./{next_folder}")
+        current_folder = f"{current_folder}/{next_folder}"
+
+    # os.system(f"cd ./{input_dir}")
